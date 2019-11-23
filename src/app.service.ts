78bf86d0a63cf08cd5b3ba4b6fd5d3c6f7ae9432
Johan Rene Garcia Monsalve <johan.garcia@outlook.com>
2019-11-23 15:20:20 -0500

    prueba2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello CI/CD prueba de actualizacion.';
+    return 'Hello CI/CD prueba de actualizacion2.';
   }
 }
